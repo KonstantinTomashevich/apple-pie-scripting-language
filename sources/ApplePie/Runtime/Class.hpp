e44760621fb9a95408099993765032e47fb85b08
Konstant <ekit.mail@gmail.com>
2016-09-30 14:06:21 +0200

    Deleted GetMembersDeclaritions () and GetFunctionsDeclaritions () from Runtime Class.

--- sources/ApplePie/Runtime/Class.hpp
+++ sources/ApplePie/Runtime/Class.hpp
@@ -1,14 +1,10 @@
 #pragma once
 #include <string>
 #include <vector>
-
 #include <ApplePie/Runtime/Object.hpp>
-#include <ApplePie/AST/VariableDeclaration.hpp>
-#include <ApplePie/AST/FunctionDeclaration.hpp>
 
 namespace ApplePie
 {
-// TODO: Why Class uses declarations? GetMembersDeclaritions and GetFunctionsDeclaritions () will be deleted, because ClassDeclarition has same functions.
 class Class : public StringifyAble, public ObjectWithKnownType
 {
 friend class Object;
@@ -20,8 +16,6 @@ public:
     Class ();
     virtual ~Class ();
 
-    virtual std::vector <VariableDeclaration *> GetMembersDeclaritions () = 0;
-    virtual std::vector <FunctionDeclaration *> GetFunctionsDeclaritions () = 0;
     virtual bool CreateInstance (Object *toObject) = 0;
     virtual bool IsInheritFrom (Class *baseClass) = 0;
 };
